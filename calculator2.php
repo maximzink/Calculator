a56d2a7101388ddba33fef5bf83e6754223d25ef
maximzink <34608161+maximzink@users.noreply.github.com>
2018-03-16 15:46:53 +0200

    Add files via upload

---
+++ calculator2.php
@@ -0,0 +1,20 @@
+
+<head>
+    <meta charset="utf-8">
+    <title>Калькулятор</title>
+<body>
+
+<form method="get" action="calculator.php">
+
+    <label for="username">Чиcло №1<br></label><input type="text" name="count1"><br>
+    <label for="username">Чиcло №2<br></label><input type="text" name="count2"><br>
+    <select name="count3">
+        <option>+</option>
+        <option>-</option>
+        <option>*</option>
+        <option>/</option>
+    </select><br><br>
+    <input type="submit" value="=">
+</form>
+</body>
+</html>
