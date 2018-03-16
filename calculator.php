b6d340abfe9a7092eb1eb70f208a3552dee5fc2d
maximzink <34608161+maximzink@users.noreply.github.com>
2018-03-16 16:01:05 +0200

    Update calculator.php

--- calculator.php
+++ calculator.php
@@ -3,7 +3,7 @@ $count1 = $_GET['count1'];
 $count2 = $_GET['count2'];
 $count3 = $_GET['count3'];
 
-echo $count1 . $count3 . $count2 . '=';
+echo $count1 ."&nbsp;". $count3 ."&nbsp;". $count2 ."&nbsp;". '='."&nbsp;";
 
 switch ($count3) {
     case '+':
